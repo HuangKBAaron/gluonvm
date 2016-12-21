24d9e3dcefc5f599e05563234bd69fe4dc8cbcc2
Dmytro Lytovchenko <dmytro.lytovchenko@gmail.com>
2016-12-21 23:18:26 +0100

    better error reporting

--- e4compile/src/e4.erl
+++ e4compile/src/e4.erl
@@ -8,7 +8,7 @@
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1, start_e4_compiler/1, start/0]).
+-export([start/2, stop/1, arguments_loop/1, start/0, try_do/2]).
 
 -include("e4.hrl").
 
@@ -20,7 +20,7 @@ start() ->
     start(normal, init:get_plain_arguments()).
 
 start(_StartType, Args) ->
-    start_e4_compiler(Args),
+    arguments_loop(Args),
     init:stop(),
     {ok, self()}.
 
@@ -31,18 +31,31 @@ stop(_State) ->
 %% Internal functions
 %%====================================================================
 
-start_e4_compiler([]) ->
+arguments_loop([]) ->
     io:format("~s~n", [color:greenb("E4: All arguments were processed")]),
     ok;
-start_e4_compiler([F | Tail]) ->
+arguments_loop([F | Tail]) ->
     io:format("E4: Processing: ~p...~n", [F]),
-    try e4_compiler:process(F)
-    catch T:Err ->
-        io:format("~n~s ~s~n"
-                  "~s~n",
-                  [color:red("E4: Failed"), ?COLOR_TERM(redb, {T, Err}),
-                   ?COLOR_TERM(blackb, erlang:get_stacktrace())
-                  ])
-    end,
-    start_e4_compiler(Tail).
+    try
+        try_do("top-level compiler invocation",
+               fun() -> e4_compiler:process(F) end),
+        arguments_loop(Tail)
+    catch throw:compile_failed ->
+        io:format("~s~n", [color:yellow("E4: Compilation failed")])
+    end.
 
+try_do(What, Fun) ->
+    try Fun()
+    catch
+        throw:compile_failed ->
+            erlang:throw(compile_failed); % chain the error out
+        T:Err ->
+            io:format("~n~s (~s): ~s~n"
+                      "~s~n",
+                      [color:red("E4: Failed"),
+                       color:yellow(What),
+                       ?COLOR_TERM(redb, {T, Err}),
+                       ?COLOR_TERM(blackb, erlang:get_stacktrace())
+                      ]),
+            erlang:throw(compile_failed)
+    end.
--- e4compile/src/e4_compiler.erl
+++ e4compile/src/e4_compiler.erl
@@ -6,15 +6,24 @@
 %% @doc Takes filename as input, produces compiled BEAM AST and processes it
 process(F) ->
     case compile:file(F, [to_kernel, binary, report]) of
-        {ok, M, CoreErlang} ->
-            Forth1      = e4_pass1:process(CoreErlang),
-            Forth2      = e4_pass2:process(Forth1),
-            RealForth1  = e4_pass3:process(Forth2),
-            RealForth2  = e4_pass_opt1:process(RealForth1),
-            J1Prog      = e4_j1c:compile(M, RealForth2),
-            IOList      = e4_file:to_iolist(J1Prog),
-            file:write_file(e4_file:bin_filename(F),
-                            iolist_to_binary(IOList));
+        {ok, M, Kernel} ->
+            IR1 = e4:try_do("Pass1 - Kernel Erlang pass",
+                            fun() -> e4_pass1:process(Kernel) end),
+            IR2 = e4:try_do("Pass2 - Variables pass",
+                            fun() -> e4_pass2:process(IR1) end),
+            RealForth1  = e4:try_do("Pass3 - Convert to Forth",
+                                    fun() -> e4_pass3:process(IR2) end),
+            RealForth2  = e4:try_do("Pass4 - Opt",
+                                    fun() -> e4_pass_opt1:process(RealForth1)
+                                    end),
+            J1Prog      = e4:try_do("Pass5 - Compile to J1 opcodes",
+                                    fun() -> e4_j1c:compile(M, RealForth2) end),
+            e4:try_do("Save binary output",
+                fun() ->
+                    IOList = e4_file:to_iolist(J1Prog),
+                    file:write_file(e4_file:bin_filename(F),
+                            iolist_to_binary(IOList))
+                end);
         E ->
             io:format("~n~s: ~p~n", [F, E])
     end.
